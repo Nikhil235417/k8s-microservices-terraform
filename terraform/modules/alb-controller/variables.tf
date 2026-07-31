847af91b22b0d6f392dd1298ca0252886cace6ed
roberto <robpalacios1@hotmail.com>
2026-07-31 14:59:43 -0500

    create variables for alb-controller module

---
+++ terraform/modules/alb-controller/variables.tf
@@ -0,0 +1,9 @@
+variable "eks_oidc_issuer_url" {
+  description = "URL from emisor of cluster EKS"
+  type = string
+}
+
+variable "cluster_name" {
+  description = "Cluster Name EKS"
+  type = string 
+}
